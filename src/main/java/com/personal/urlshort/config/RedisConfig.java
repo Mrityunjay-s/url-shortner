7029576aad58bc2b82418c3841bf7b40e71bf29f
mrityunjay <mrityunjayprasad.singh@yokogawa.com>
2026-09-11 14:09:32 +0530

    removed unused lines

--- src/main/java/com/personal/urlshort/config/RedisConfig.java
+++ src/main/java/com/personal/urlshort/config/RedisConfig.java
@@ -3,12 +3,53 @@ package com.personal.urlshort.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.client.RestTemplate;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import tools.jackson.databind.jsontype.PolymorphicTypeValidator;
 
 @Configuration
 public class RedisConfig {
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
         return new StringRedisTemplate(factory);
+    }
+
+    @Bean
+    public RedisSerializer<Object> redisValueSerializer() {
+
+        PolymorphicTypeValidator ptv =
+                BasicPolymorphicTypeValidator.builder()
+                        .allowIfSubType("com.personal.urlshort.dto")
+                        .allowIfSubType("java.util")
+                        .build();
+
+        return GenericJacksonJsonRedisSerializer.builder()
+                .enableDefaultTyping(ptv)
+                .build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> restTemplate(
+            RedisConnectionFactory factory,
+            RedisSerializer<Object> redisSerializer
+    ) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        template.setValueSerializer(redisSerializer);
+        template.setHashValueSerializer(redisSerializer);
+
+        template.afterPropertiesSet();
+
+        return template;
     }
 }
--- src/main/java/com/personal/urlshort/constants/RedisConstants.java
+++ src/main/java/com/personal/urlshort/constants/RedisConstants.java
@@ -1,9 +1,13 @@
 package com.personal.urlshort.constants;
 
+import java.time.Duration;
+
 public class RedisConstants {
+
     private RedisConstants() {
     }
 
-    public static final String URL_COUNTER = "urlshortener:counter";
-    public static final String URL_PREFIX = "urlshortener:url:";
+    public static final String URL_COUNTER = "shortener:counter";
+    public static final String URL_PREFIX = "shortener:url:";
+    public static final Duration DEFAULT_TTL = Duration.ofMinutes(10);
 }
